e66df9959d9bc43ffc9e5e4effbeb4d824e5880c
julianazadarko <43300146+julianazadarko@users.noreply.github.com>
2019-09-15 08:12:07 -0400

    Arduino-Unity connection

---
+++ HTN_V1/Assets/Tutscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Tutscript : MonoBehaviour {
+
+    public float speed;
+    public float amountToMove;
+
+    SerialPort sp = new SerialPort("COM6", 9600);
+
+    // Use this for initialization
+    void Start () {
+        sp.Open();
+        sp.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        amountToMove = speed * Time.deltaTime;
+
+        if (sp.IsOpen)
+        {
+            try
+            {
+                MoveObject(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch(System.Exception)
+            {
+            }
+        }
+	}
+
+    void MoveObject(int Direction)
+    {
+        if (Direction == 1)
+        {
+            transform.Translate(Vector3.left * amountToMove, Space.World);
+        }
+        if (Direction == 2)
+        {
+            transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+    }
+}
